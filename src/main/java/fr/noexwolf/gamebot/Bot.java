deefb720e7b769be1d5aeeeec06a4582fecd290f
NoeXWolf <noe.mastrorillo@gmail.com>
2020-05-14 19:43:58 +0200

    Adding JDA-Utilities stuff

--- src/main/java/fr/noexwolf/gamebot/Bot.java
+++ src/main/java/fr/noexwolf/gamebot/Bot.java
@@ -1,5 +1,7 @@
 package fr.noexwolf.gamebot;
 
+import com.jagrosh.jdautilities.command.CommandClient;
+import com.jagrosh.jdautilities.command.CommandClientBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
@@ -13,9 +15,14 @@ public class Bot {
     private final JDA jda;
 
     public Bot(String token) throws LoginException {
+        CommandClient commandBuilder = new CommandClientBuilder()
+                .setPrefix("+")
+                .build();
+
         jda = JDABuilder.create(token, GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MESSAGE_REACTIONS)
                 .setMemberCachePolicy(MemberCachePolicy.NONE)
                 .disableCache(CacheFlag.VOICE_STATE, CacheFlag.EMOTE, CacheFlag.ACTIVITY, CacheFlag.CLIENT_STATUS)
+                .addEventListeners(commandBuilder)
                 .build();
     }
 
