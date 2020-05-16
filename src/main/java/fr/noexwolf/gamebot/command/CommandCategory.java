8ebe633975ef56941b9fc327fb83065340f7822a
NoeXWolf <noe.mastrorillo@gmail.com>
2020-05-16 19:23:59 +0200

    Change command category system to jda-utilities one

--- src/main/java/fr/noexwolf/gamebot/command/CommandCategory.java
+++ src/main/java/fr/noexwolf/gamebot/command/CommandCategory.java
@@ -1,24 +1,20 @@
 package fr.noexwolf.gamebot.command;
 
+import com.jagrosh.jdautilities.command.Command;
+
 public enum CommandCategory {
-    DEFAULT("Default", "Basic commands"),
-    GAME("Game", "Game commands"),
-    ECONOMY("Economy", "Economy commands");
+    DEFAULT(new Command.Category("Default")),
+    GAME(new Command.Category("Game")),
+    ECONOMY(new Command.Category("Economy"));
 
-    private final String name;
-    private final String description;
+    private final Command.Category category;
 
-    CommandCategory(String name, String description) {
-        this.name = name;
-        this.description = description;
+    CommandCategory(Command.Category category) {
+        this.category = category;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
+    public Command.Category getCategory() {
+        return category;
     }
 
 }
--- src/main/java/fr/noexwolf/gamebot/command/commands/TestCommand.java
+++ src/main/java/fr/noexwolf/gamebot/command/commands/TestCommand.java
@@ -2,19 +2,17 @@ package fr.noexwolf.gamebot.command.commands;
 
 import com.jagrosh.jdautilities.command.CommandEvent;
 import fr.noexwolf.gamebot.command.Command;
-import fr.noexwolf.gamebot.command.arguments.Argument;
-import fr.noexwolf.gamebot.command.arguments.ArgumentBuilder;
+import fr.noexwolf.gamebot.command.CommandCategory;
 import fr.noexwolf.gamebot.command.arguments.IntArgument;
 
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 public class TestCommand extends Command {
 
     public TestCommand() {
         this.name = "test";
         this.help = "Test command to test the command system";
+        this.category = CommandCategory.DEFAULT.getCategory();
         this.arguments = Collections.singletonList(new IntArgument("number", true));
     }
 
