c7cbb9274ab16fd999c2538697ae7df624f1096b
NoeXWolf <noe.mastrorillo@gmail.com>
2020-05-24 20:35:11 +0200

    Some changes

--- src/main/java/fr/noexwolf/gamebot/command/settings/GuildSettingsProviderImpl.java
+++ src/main/java/fr/noexwolf/gamebot/command/settings/GuildSettingsProviderImpl.java
@@ -1,11 +1,26 @@
 package fr.noexwolf.gamebot.command.settings;
 
 import com.jagrosh.jdautilities.command.GuildSettingsProvider;
+import fr.noexwolf.gamebot.Bot;
 import net.dv8tion.jda.api.entities.Guild;
+
+import java.sql.PreparedStatement;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public class GuildSettingsProviderImpl implements GuildSettingsProvider {
 
-//    public String getPrefixByGuild(Guild guild) {
+    private final Guild guild;
+    private final Bot bot;
+
+    public GuildSettingsProviderImpl(Guild guild, Bot bot) {
+        this.guild = guild;
+        this.bot = bot;
+    }
+
+//    @Override
+//    public Collection<String> getPrefixes() {
 //
 //    }
 
